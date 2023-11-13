2036c9dfb9f74df53b0dd65711805addffcefc31
IB29TUM <104312343+IB29TUM@users.noreply.github.com>
2023-11-14 00:37:29 +0100

    Update socketlogic.js

--- socketlogic.js
+++ socketlogic.js
@@ -9,13 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     socket.onmessage = function(event) {
         const data = event.data;
-        // Assuming the server sends data in a similar format as your Python script
-        // const hexStringMappings = {
-        //     "3400476C61737320202020202020": "Glass",
-        //     "3400436F6C6120424F54544C4520": "CocaCola Bottle",
-        //     "3400426F78202020202020202020": "Box",
-        //     "3000E2801191A5030065B837C026": "Power Adapter"
-        // };
+
 
 // const cleanedData = data.replace('\x02', '').replace('\x03', '').trim();
 // const displayText = hexStringMappings[cleanedData] || `Received data: ${cleanedData}`;
